48e560884ec6235b7ddbace42cea291b55738519
Kazuya Tateishi <k-tateishi@tandems.co.jp>
2023-06-05 10:42:06 +0900

    ログイン認証

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ ruby '3.2.2'
 
 gem 'rails', '~> 7.0.4.2'
 
+gem 'devise'
 gem 'mysql2', '~> 0.5'
 gem 'puma', '~> 5.0'
 gem 'sprockets-rails'
@@ -15,6 +16,8 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 group :development, :test do
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'ruby-debug-ide'
+  gem 'debase', '0.2.5.beta2'
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "active_model/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
-# require "action_mailer/railtie"
+require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require "action_view/railtie"
@@ -34,6 +34,7 @@ module App
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.time_zone = 'Tokyo'
+    config.action_mailer.default_url_options = { host: ENV['APP_DEFAULT_URL_HOST'], port: ENV['APP_DEFAULT_URL_PORT'] }
     Rails.application.routes.default_url_options[:host] = ENV['APP_DEFAULT_URL_HOST']
     Rails.application.routes.default_url_options[:port] = ENV['APP_DEFAULT_URL_PORT']
   end
